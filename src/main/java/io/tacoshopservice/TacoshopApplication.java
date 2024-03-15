64491bce7b1da6a0a5f7fb342b16be5d26dd67d9
jh <jadehl09@gmail.com>
2024-03-15 23:42:45 +0900

    init commit

---
+++ src/main/java/io/tacoshopservice/TacoshopApplication.java
@@ -0,0 +1,12 @@
+package io.tacoshopservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacoshopApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(TacoshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/tacoshopservice/TacoshopApplicationTests.java
@@ -0,0 +1,12 @@
+package io.tacoshopservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacoshopApplicationTests {
+	@Test
+	void contextLoads() {
+	}
+
+}
