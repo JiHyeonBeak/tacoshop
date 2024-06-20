8cadddbfa03664502249199aee223f313d2eb06c
jh <jadehl09@gmail.com>
2024-06-20 18:14:14 +0900

    add VO Dice

---
+++ src/main/java/io/tacoshopservice/main/domain/Dice.java
@@ -0,0 +1,19 @@
+package io.tacoshopservice.main.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class Dice {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String tacosName;
+
+}
