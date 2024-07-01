a1e28dbf8fc299a6e207c1fea407dd89abffc99a
jh <jadehl09@gmail.com>
2024-07-01 11:27:32 +0900

    seperating directory

--- src/main/java/io/tacoshopservice/main/web/controller/MainController.java
+++ src/main/java/io/tacoshopservice/main/web/controller/MainController.java
@@ -1,4 +1,4 @@
-package io.tacoshopservice.main.controller;
+package io.tacoshopservice.main.web.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
